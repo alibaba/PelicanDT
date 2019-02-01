738cea5a3e3fcecd25a2421cbe2055f35770b04a
墨昀 <junyang.zjy@alibaba-inc.com>
2019-02-01 17:26:39 +0800

    增加dubbo部署并执行测试功能

--- Demo-example/src/main/java/com/alibaba/pelican/demo/DemoExecScript.java
+++ Demo-example/src/main/java/com/alibaba/pelican/demo/DemoExecScript.java
@@ -26,7 +26,7 @@ public class DemoExecScript {
         config.setPassword(password);
 
         RemoteCmdClient client = new RemoteCmdClient(config);
-        RemoteCmdResult result = client.scpAndExecScript("demo.sh");
+        RemoteCmdResult result = client.scpAndExecScript("demo1.sh");
         System.out.println(result.getStdInfo());
     }
 }
---
+++ Dubbo-example/README.md
@@ -0,0 +1,3 @@
+# PelicanDT In Alibaba
+
+## 基于RocketMQ编写的集成测试用例
